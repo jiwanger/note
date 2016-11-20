1f2c9d716df66b43bbd7e0658536d74589d79463
jiwanger <jiwanger@126.com>
2016-11-20 10:07:04 +0800

    删除eclipse文件同步

--- web/res/menu.js
+++ web/res/menu.js
@@ -1,6 +1,6 @@
 $(function() {
 	var arr = [ "index.html", "HTML", "css.html", "CSS", "jquery.html",
-			"JQUERY", "jascript.html", "JAVASCRIPT", "bootstrap.html",
+			"JQUERY", "javascript.html", "JAVASCRIPT", "bootstrap.html",
 			"BOOTSTRAP", "html5.html", "HTML5", "node-js.html", "NODE-JS",
 			"markdown.html", "MARKDOWN", "jquery-ui.html", "JQUERY-UI",
 			"jquery-ui-bootstrap.html", "JQUERY-UI-BOOTSTRAP", "muse-ui.html", "MUSE-UI" ];
