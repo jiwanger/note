49ed10c6243fc28e2644ecfb5df0079b0b35fdd9
jiwanger <jiwanger@126.com>
2016-11-24 09:26:36 +0800

    menu.js修改

--- commons/js/common.js
+++ commons/js/common.js
@@ -1,8 +1,29 @@
 SyntaxHighlighter.all();
 
+function init_menu(arr) {
+	var pathname = window.location.pathname;
+
+	var pagename = pathname.substring(pathname.lastIndexOf("/") + 1);
+
+	for (i = 0; i < arr.length; i += 2) {
+		var element = "";
+		if (pagename == arr[i]) {
+			element = "<li class=\"active\"><a href=\"#\">" + arr[i + 1]
+					+ "</a></li>";
+		} else {
+			element = "<li><a href=\"" + arr[i] + "\">" + arr[i + 1]
+					+ "</a></li>";
+		}
+		$("#my-nav").append(element);
+	}
+}
+
 $(function(){
+	init_menu(arr);
+	
 	$(".container .col-md-3 .list-group .active").on("click", function(){
 		$(this).toggleClass("dropdown");
 		$(this).toggleClass("dropup");
 	});
+	
 });
--- web/res/menu.js
+++ web/res/menu.js
@@ -1,24 +1,5 @@
-$(function() {
-	var arr = [ "index.html", "HTML", "css.html", "CSS", "jquery.html",
-			"JQUERY", "javascript.html", "JAVASCRIPT", "bootstrap.html",
-			"BOOTSTRAP", "html5.html", "HTML5", "node-js.html", "NODE-JS",
-			"markdown.html", "MARKDOWN", "jquery-ui.html", "JQUERY-UI",
-			"jquery-ui-bootstrap.html", "JQUERY-UI-BOOTSTRAP" ];
-
-	var pathname = window.location.pathname;
-
-	var pagename = pathname.substring(pathname.lastIndexOf("/") + 1);
-
-	for (i = 0; i < arr.length; i += 2) {
-		var element = "";
-		if (pagename == arr[i]) {
-			element = "<li class=\"active\"><a href=\"#\">" + arr[i + 1]
-					+ "</a></li>";
-		} else {
-			element = "<li><a href=\"" + arr[i] + "\">" + arr[i + 1]
-					+ "</a></li>";
-		}
-		$("#my-nav").append(element);
-	}
-
-});
+var arr = [ "index.html", "HTML", "css.html", "CSS", "jquery.html", "JQUERY",
+		"javascript.html", "JAVASCRIPT", "bootstrap.html", "BOOTSTRAP",
+		"html5.html", "HTML5", "node-js.html", "NODE-JS", "markdown.html",
+		"MARKDOWN", "jquery-ui.html", "JQUERY-UI", "jquery-ui-bootstrap.html",
+		"JQUERY-UI-BOOTSTRAP" ];
