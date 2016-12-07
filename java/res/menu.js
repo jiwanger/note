daec02fd99237787dc8e50167e6909f4554d9120
jiwanger <jiwanger@126.com>
2016-12-07 09:14:14 +0800

    20161207

--- java/res/menu.js
+++ java/res/menu.js
@@ -1,4 +1,4 @@
-var arr = [ "index.html", "JAVA", "commons.html", "CONMONS", "j2ee.html",
-		"J2EE", "tomcat.html", "TOMCAT", "struts.html", "STRUTS",
-		"spring.html", "SPRING", "hibernate.html", "HIBERNATE", "mybatis.html",
-		"MYBATIS"];
+var arr = [ "index.html", "JAVA", "commons.html", "CONMONS", "model.html",
+		"MODEL", "j2ee.html", "J2EE", "tomcat.html", "TOMCAT", "struts.html",
+		"STRUTS", "spring.html", "SPRING", "hibernate.html", "HIBERNATE",
+		"mybatis.html", "MYBATIS" ];
