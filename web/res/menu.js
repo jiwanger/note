1c63a182c79f6ae455ddfae479e076df4383b6dd
jiwanger <jiwanger@126.com>
2016-12-16 17:02:06 +0800

    20161215

--- web/res/menu.js
+++ web/res/menu.js
@@ -1,5 +1,5 @@
 var arr = [ "index.html", "HTML", "css.html", "CSS", "jquery.html", "JQUERY",
 		"javascript.html", "JAVASCRIPT", "bootstrap.html", "BOOTSTRAP",
-		"html5.html", "HTML5", "node-js.html", "NODE-JS", "markdown.html",
-		"MARKDOWN", "jquery-ui.html", "JQUERY-UI", "jquery-ui-bootstrap.html",
-		"JQUERY-UI-BOOTSTRAP", "fontawesome.html", "ASESOME" ];
+		"html5.html", "HTML5", "node-js.html", "NODE-JS", "node-webkit.html",
+		"NODE-WEBKIT", "markdown.html", "MARKDOWN", "jquery-ui.html",
+		"JQUERY-UI", "fontawesome.html", "ASESOME" ];
