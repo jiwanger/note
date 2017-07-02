40e7574023a5111cd4afea57f2ae3e7f5852bc52
jiwanger <jiwanger@126.com>
2017-07-03 07:38:49 +0800

    gernaral

--- java/res/menu.js
+++ java/res/menu.js
@@ -1,4 +1,4 @@
-var arr = [ "index.html", "JAVA", "jdk.html", "JDK", "commons.html", "CONMONS",
+var arr = [ "index.html", "JAVA", "jdk.html", "JDK", "commons.html", "LIBS",
 		"model.html", "MODEL", "j2ee.html", "J2EE", "tomcat.html", "TOMCAT",
 		"struts.html", "STRUTS", "spring.html", "SPRING", "hibernate.html",
 		"HIBERNATE", "mybatis.html", "MYBATIS" ];
