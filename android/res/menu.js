c56b1de8406b59aec626a4335708d26edaa92e78
jiwanger <jiwanger@126.com>
2017-11-14 10:55:07 +0800

    android.graphics added!

--- android/res/menu.js
+++ android/res/menu.js
@@ -1,3 +1,3 @@
 var arr = [ "index.html", "ANDROID", "src.html", "SOURCE-CODE", "adb.html", "ADB", "manifest.html",
-		"MANIFEST", "view.html", "VIEW", "drawable.html", "DRAWABLE",
+		"MANIFEST", "view.html", "VIEW", "drawable.html", "DRAWABLE", "graphics.html", "GRAPHICS",
 		"animation.html", "ANIMATION", "libs.html", "LIBS" ];
