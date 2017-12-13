635b76df7c3649d34f3cad96f2709f619b6c8895
jiwanger <jiwanger@126.com>
2017-12-13 08:35:36 +0800

    android-sdk

--- android/res/menu.js
+++ android/res/menu.js
@@ -1,3 +1,4 @@
-var arr = [ "index.html", "ANDROID", "src.html", "SOURCE-CODE", "adb.html", "ADB", "manifest.html",
-		"MANIFEST", "view.html", "VIEW", "drawable.html", "DRAWABLE", "graphics.html", "GRAPHICS",
-		"animation.html", "ANIMATION", "libs.html", "LIBS" ];
+var arr = [ "index.html", "ANDROID", "src.html", "SOURCE-CODE", "adb.html",
+		"ADB", "manifest.html", "MANIFEST", "sdk.html","SDK", "graphics.html", "GRAPHICS",
+		"drawable.html", "DRAWABLE", "view.html", "VIEW", "animation.html",
+		"ANIMATION", "libs.html", "LIBS" ];
