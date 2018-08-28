ccc34e06b4bf0c97acd0c99e5056e10ab25568d2
jiwanger <jiwanger@126.com>
2018-08-28 18:04:34 +0800

    AWT

--- java/res/menu.js
+++ java/res/menu.js
@@ -1,4 +1,4 @@
-var arr = [ "index.html", "JAVA", "jdk.html", "JDK", "commons.html", "LIBS",
+var arr = [ "index.html", "JAVA", "jdk.html", "JDK", "swing.html", "SWING", "commons.html", "LIBS",
 		"model.html", "MODEL", "j2ee.html", "J2EE", "tomcat.html", "TOMCAT",
 		"spring.html", "SPRING", "mybatis.html", "MYBATIS", "struts.html",
 		"STRUTS", "hibernate.html", "HIBERNATE" ];
