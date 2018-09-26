6b270df5c7a3fa08f59175a8fa31ce6b8afeca04
jiwanger <jiwanger@126.com>
2018-09-26 16:16:08 +0800

    JavaEE

--- java/res/menu.js
+++ java/res/menu.js
@@ -1,4 +1,4 @@
 var arr = [ "index.html", "JAVA", "jdk.html", "JDK", "swing.html", "JavaFX", "commons.html", "LIBS",
-		"model.html", "MODEL", "j2ee.html", "J2EE", "tomcat.html", "TOMCAT",
+		"model.html", "MODEL", "j2ee.html", "JavaEE", "tomcat.html", "TOMCAT",
 		"spring.html", "SPRING", "mybatis.html", "MYBATIS", "struts.html",
 		"STRUTS", "hibernate.html", "HIBERNATE" ];
