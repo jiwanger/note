076c601cfe2755b4b04d8b49b603032ed587bca5
jiwanger <jiwanger@126.com>
2021-07-09 16:55:54 +0800

    javascript

--- web/js/demo-js.js
+++ web/js/demo-js.js
@@ -1,30 +1,35 @@
 "use strict";
 
-$(function() {
+const uri = "http://x.com:80/path/file.php?x=1&y=你好&z=s(a + b)#anchor";
 
-    new Promise(function (resolve, reject) {
-        setTimeout(function () {
-            console.log("First");
-            resolve();
-        }, 1000);
-    }).then(function () {
-        return new Promise(function (resolve, reject) {
-            setTimeout(function () {
-                console.log("Second");
-                resolve();
-            }, 4000);
-        });
-    }).then(function () {
-        setTimeout(function () {
-            console.log("Third");
-        }, 3000);
-    });
-})
+const encodedURI = encodeURI(uri);
+console.log(encodedURI);
+const encodedURIComponent = encodeURIComponent(uri);
+console.log(encodedURIComponent);
 
-var jiwanger = {
-    name : "Jiwanger",
-    age: 31,
-    info: function() {
-        return "Name is " + this.name + ", Age is " + this.age + "."
-    }
-};
+console.log(uri === decodeURI(encodedURI));
+console.log(uri === decodeURIComponent(encodedURIComponent));
+
+// 多次处理
+console.log(encodeURI(encodedURI));
+console.log(encodeURIComponent(encodedURIComponent));
+
+// 多次处理过的数据需要多次还原
+console.log(uri === decodeURI(decodeURI(encodeURI(encodedURI))));       // true
+console.log(uri === decodeURIComponent(decodeURIComponent(encodeURIComponent(encodedURIComponent)))); // true
+
+/** encodeURIComponent() 和 encodeURI 有以下几个不同点 */
+var set1 = ";,/?:@&=+$";  // 保留字符
+var set2 = "-_.!~*'()";   // 不转义字符
+var set3 = "#";           // 数字标志
+var set4 = "ABC abc 123"; // 字母数字字符和空格
+
+console.log(encodeURI(set1)); // ;,/?:@&=+$
+console.log(encodeURI(set2)); // -_.!~*'()
+console.log(encodeURI(set3)); // #
+console.log(encodeURI(set4)); // ABC%20abc%20123 (the space gets encoded as %20)
+
+console.log(encodeURIComponent(set1)); // %3B%2C%2F%3F%3A%40%26%3D%2B%24
+console.log(encodeURIComponent(set2)); // -_.!~*'()
+console.log(encodeURIComponent(set3)); // %23
+console.log(encodeURIComponent(set4)); // ABC%20abc%20123 (the space gets encoded as %20)
--- web/res/menu.js
+++ web/res/menu.js
@@ -1,4 +1,4 @@
 var arr = [ "index.html", "HTML", "css.html", "CSS", 
-		"javascript.html", "JAVASCRIPT", "jquery.html", "JQUERY", "bootstrap.html", "BOOTSTRAP",
+		"javascript.html", "JAVASCRIPT", "typescript.html","TYPESCRIPT","jquery.html", "JQUERY", "bootstrap.html", "BOOTSTRAP",
 		"html5.html", "HTML5", "node-js.html", "NODE-JS", "markdown.html", "MARKDOWN", "jquery-ui.html",
 		"JQUERY-UI", "fontawesome.html", "ASESOME", "material.html", "MATERIAL" ];
